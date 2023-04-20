397569eb5fde38263cb1327ec3ce378df3ed557a
Tomer Israeli <tomer_israeli@outlook.co.il>
2023-04-20 22:36:23 +0300

    added some basic project structure

---
+++ ashes_and_dust.py
@@ -0,0 +1,6 @@
+class AshesAndDust:
+    """
+    the main class of the project, provides an easy way to run the different functions of the project
+    """
+
+    #NOTE: all the functions of this class should be static
