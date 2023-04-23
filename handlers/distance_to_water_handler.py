3a67c0f0e7697cbeff3a3cd5d17daf4db446bc3a
Tomer Israeli <tomer_israeli@outlook.co.il>
2023-04-23 22:43:21 +0300

    added hanlders to constants.py

--- handlers/distance_to_water_handler.py
+++ handlers/distance_to_water_handler.py
@@ -3,7 +3,7 @@ from handlers.tif_handler import TifHandler
 import os
 
 
-class DistanceFromWaterHandler(TifHandler):
+class DistanceToWaterHandler(TifHandler):
     NAME = "distance_to_water"
     SOURCE = "local"
     TIF_PATH = os.path.join("distance_to_water", "distance_to_water.tif")
@@ -11,6 +11,6 @@ class DistanceFromWaterHandler(TifHandler):
 
 if __name__ == '__main__':
     root_path = "/Users/tomerisraeli/Library/CloudStorage/GoogleDrive-tomer.israeli.43@gmail.com/My Drive/year_2/Magdad/data_preprocess"
-    handler = DistanceFromWaterHandler()
+    handler = DistanceToWaterHandler()
     print(handler.confirm_existence(root_path))
     handler.preprocess(root_path)
--- utils/constants.py
+++ utils/constants.py
@@ -1,6 +1,9 @@
+from handlers.distance_to_water_handler import DistanceToWaterHandler
+
 DATE_FORMATS = formats = ["%Y",
                           "%m/%Y", "%m-%Y", "%m.%Y",
                           "%d/%m/%Y", "%d-%m-%Y", "%d.%m.%Y"
                           ]
-HANDLERS = ["DistanceFromWaterHandler"]
-REQUIRED_FILES = ['h20v05.shp', 'h21v05.shp', 'h21v06.shp', 'h20v05.tif', 'h21v05.tif', 'h21v06.tif']
+
+LOCAL_HANDLERS = [DistanceToWaterHandler()]
+DOWNLOAD_HANDLERS = []
