fb771d696e56f6b21911cf216593919a856e64cf
Tomer Israeli <tomer_israeli@outlook.co.il>
2023-05-05 19:10:09 +0300

    improved running time of population_density_handler_support.py

--- handlers/local_handlers/population_density/population_density_handler_support.py
+++ handlers/local_handlers/population_density/population_density_handler_support.py
@@ -3,19 +3,24 @@ from functools import cache
 from osgeo import gdal, osr
 from osgeo import ogr
 import numpy as np
-from rich.progress import track
+from rich import progress
+from rich.progress import track, Progress
+from tqdm import tqdm
 
 
-def __generate_ring(px, py, geotransform):
+@cache
+def __get_cell_geometry(col, row, geotransform):
     """
     generate the ring of the given coordinate
 
 
-    :param px: the x value of the pixel
-    :param py: the y value of the pixel
+    :param row:
+    :param col:
     :param geotransform: the geotransform of the reference raster
     :return:
     """
+
+    px, py = __calc_cell_dimensions(col, row, geotransform)
 
     ring = ogr.Geometry(ogr.wkbLinearRing)
     ring.AddPoint(px - geotransform[1] / 2, py - geotransform[5] / 2)
@@ -23,7 +28,10 @@ def __generate_ring(px, py, geotransform):
     ring.AddPoint(px + geotransform[1] / 2, py + geotransform[5] / 2)
     ring.AddPoint(px - geotransform[1] / 2, py + geotransform[5] / 2)
     ring.AddPoint(px - geotransform[1] / 2, py - geotransform[5] / 2)
-    return ring
+
+    cell_geom = ogr.Geometry(ogr.wkbPolygon)
+    cell_geom.AddGeometry(ring)
+    return cell_geom
 
 
 def __calc_cell_dimensions(x, y, geotransform):
@@ -43,76 +51,6 @@ def __calc_cell_dimensions(x, y, geotransform):
     )
 
 
-@cache
-def __get_tif_geometry(reference):
-    """
-    get the reference tif geometry
-
-
-    :param reference:
-    :return:
-    """
-
-    gt = reference.GetGeoTransform()
-    # Get the width and height of the raster
-    cols = reference.RasterXSize
-    rows = reference.RasterYSize
-
-    # Get the corner coordinates
-    min_x = gt[0]
-    max_y = gt[3]
-    max_x = min_x + gt[1] * cols
-    min_y = max_y + gt[5] * rows
-
-    # Create a polygon geometry using the corner coordinates
-    ring = ogr.Geometry(ogr.wkbLinearRing)
-    ring.AddPoint(min_x, min_y)
-    ring.AddPoint(max_x, min_y)
-    ring.AddPoint(max_x, max_y)
-    ring.AddPoint(min_x, max_y)
-    ring.AddPoint(min_x, min_y)
-    poly = ogr.Geometry(ogr.wkbPolygon)
-    poly.AddGeometry(ring)
-    return poly
-
-
-def __is_feature_relevant(feature, raster_ds, field):
-    """
-    check if the given feature is relevant to the raster
-
-
-    :param feature:
-    :param raster_ds:
-    :param field:
-    :return:
-    """
-
-    poly_geom = feature.geometry()
-    tif_geom = __get_tif_geometry(raster_ds)
-    return poly_geom.Intersects(tif_geom) and (feature.GetFieldIndex(field) != -1)
-
-
-def __calc_cell_value(features, cell_geom, field):
-    """
-    calc the new cell value
-
-
-    :param features:
-    :param cell_geom:
-    :param field:
-    :return:
-    """
-    pixel_value = np.float32(0)
-    for feature in features:
-        poly_geom = feature.geometry()
-        if poly_geom.Intersects(cell_geom):
-            intersection = poly_geom.Intersection(cell_geom)
-            weight = intersection.Area() / poly_geom.Area()
-            value = feature.GetField(field)
-            pixel_value += np.float32(weight * value)
-    return pixel_value
-
-
 def polygons_to_raster(raster_path, polygon_path, field, output_raster):
     """
     convert the given polygons to a raster matching the geographic characteristic of the reference raster
@@ -125,47 +63,81 @@ def polygons_to_raster(raster_path, polygon_path, field, output_raster):
     :return: None
     """
 
+    # to make the conversion faster, the code loops over all the polygons and finds the cells that a relevant to
+    # this polygon instead of finding the polygons that are relevant to the cell which is harder
+
     # Load the input raster and vector files
     raster_ds = gdal.Open(raster_path)
     vector_ds = ogr.Open(polygon_path)
+
     layer = vector_ds.GetLayer()
 
-    # Get the raster geotransform and projection
-    geotransform = raster_ds.GetGeoTransform()
+    # get some basic value of the reference raster
+    transform = raster_ds.GetGeoTransform()
     projection = raster_ds.GetProjection()
 
-    features = [feature for feature in layer if __is_feature_relevant(feature, raster_ds, field)]
-    # print(f"filtered {len(layer) - len(features)} from {len(layer)}")
-    output_data = np.zeros((raster_ds.RasterYSize, raster_ds.RasterXSize))
+    x_origin = transform[0]
+    y_origin = transform[3]
+    pixel_width = transform[1]
+    pixel_height = transform[5]
+    rows = raster_ds.RasterYSize
+    cols = raster_ds.RasterXSize
 
-    for y in track(range(raster_ds.RasterYSize),
-                   description=f"converting polygons to .tif at {raster_path}"):
-        for x in range(raster_ds.RasterXSize):
-            # Get the coordinates of the current cell
-            px, py = __calc_cell_dimensions(x, y, geotransform)
-            ring = __generate_ring(px, py, geotransform)
+    # generate all the cells coordinates in the raster
+    output_data = np.zeros((rows, cols))
+    dist = (pixel_width ** 2 + pixel_height ** 2) ** 0.5  # the max distance between two points in a cell
 
-            cell_geom = ogr.Geometry(ogr.wkbPolygon)
-            cell_geom.AddGeometry(ring)
+    cells = []
+    for row in range(rows):
+        for col in range(cols):
+            x = transform[0] + col * transform[1] + row * transform[2]
+            y = transform[3] + col * transform[4] + row * transform[5]
+            cells.append((row, col, x, y))
 
-            output_data[y][x] = __calc_cell_value(features, cell_geom, field)
+    with Progress() as progress:
+        features_iteration_progress = progress.add_task("[bold]converting polygons...", total=layer.GetFeatureCount())
+        for feature in layer:
+            progress.update(features_iteration_progress, advance=1)
+            if feature.GetFieldIndex(field) == -1:
+                # features doesn't hold the wanted field, no need to ran than
+                continue
+
+            geometry = feature.GetGeometryRef()
+            x_min, x_max, y_min, y_max = geometry.GetEnvelope()
+
+            # to make sure we don't miss any cell, we will make the bbox bigger at each direction
+            x_min -= dist
+            x_max += dist
+            y_min -= dist
+            y_max += dist
+
+            # we now want to get all the cells that are in the bounding box
+            filtered_cells = [cell for cell in cells if x_min <= cell[2] <= x_max and y_min <= cell[3] <= y_max]
+
+            feature_intersections_progress = progress.add_task("[grey]current polygon calculation ",
+                                                               total=len(filtered_cells))
+            value = feature.GetField(field)
+            feature_area = geometry.Area()
+
+            for row, col, _, _ in filtered_cells:
+                progress.update(feature_intersections_progress, advance=1)
+
+                cell_geom = __get_cell_geometry(col, row, transform)
+
+                intersection = geometry.Intersection(cell_geom)
+                weight = intersection.Area() / feature_area
+                output_data[row][col] += np.float32(weight * value)
+
+            progress.remove_task(feature_intersections_progress)
 
     # Create the output raster file
     driver = gdal.GetDriverByName("GTiff")
     output_ds = driver.Create(output_raster, raster_ds.RasterXSize, raster_ds.RasterYSize, 1, gdal.GDT_Float32)
-    output_ds.SetGeoTransform(geotransform)
+    output_ds.SetGeoTransform(transform)
     output_ds.SetProjection(projection)
     output_band = output_ds.GetRasterBand(1)
     output_band.WriteArray(output_data)
     output_ds.FlushCache()
 
+    # free objects and close them
     raster_ds, vector_ds, output_raster = None, None, None
-
-
-if __name__ == '__main__':
-    polygons_to_raster(
-        raster_path="/Users/tomerisraeli/Documents/GitHub/ashes-and-dust-cli/handlers/local_handlers/test/h21v05.tif",
-        polygon_path="/Users/tomerisraeli/Documents/GitHub/ashes-and-dust-cli/handlers/local_handlers/test/LAMS2011_2039.shp",
-        field="Pop_Total",
-        output_raster="/Users/tomerisraeli/Documents/GitHub/ashes-and-dust-cli/handlers/local_handlers/test/out.tif"
-    )
