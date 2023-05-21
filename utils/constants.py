4d746416dcb5f657f0a36bf90dcb1aeafd749940
Tomer Israeli <tomer_israeli@outlook.co.il>
2023-05-21 17:44:51 +0300

    fixed some git problems after the last merge

--- utils/constants.py
+++ utils/constants.py
@@ -1,9 +1,11 @@
+from data_handlers.download_handlers.pbl_handler import PBLHandler
+
 from data_handlers.local_handlers.distance_to_water_handler import DistanceToWaterHandler
 from data_handlers.local_handlers.elevation_handler import ElevationHandler
 from data_handlers.local_handlers.land_use_handler import LandUseHandler
 from data_handlers.local_handlers.population_density.population_density_handler import PopulationDensityHandler
 from data_handlers.local_handlers.roads_handler import RoadsHandler
-from handlers.download_handlers.pbl_handler import PBLHandler
+
 from utils.configuration_values import ConfigurationValues
 
 DATE_FORMATS = formats = ["%Y",
