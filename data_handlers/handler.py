49e9a36893bc28b47894485fade8444c5f0919d9
Tomer Israeli <tomer_israeli@outlook.co.il>
2023-05-26 11:20:29 +0300

    added get required files to handler api

--- data_handlers/handler.py
+++ data_handlers/handler.py
@@ -6,9 +6,9 @@ class Handler:
     the general handler api of the program. provides the basic structure of a data-handler on the app.
     """
 
-    SOURCE: str = ...       # the source of the data handled by the Handler
-    NAME: str = ...         # a short name for data handled by the Handler
-    DESCRIPTION: str = ""   # a short description of the data
+    SOURCE: str = ...  # the source of the data handled by the Handler
+    NAME: str = ...  # a short name for data handled by the Handler
+    DESCRIPTION: str = ""  # a short description of the data
 
     CLIP_AND_REPROJECT_FILES = [
         # (clip file - shp, reproject file - tif, tile name)
@@ -22,6 +22,14 @@ class Handler:
         preprocess the raw data: clip, reproject and save as a new netcdf file for each tile
         :param path:    the path to the root dir holding all data of the project
         :return:        None
+        """
+        ...
+
+    def get_required_files_list(self, root_dir):
+        """
+        get a list of all the required files for the handler functions
+        :param root_dir:    the path to root dir, might not be a valid path
+        :return:            a list of paths as str
         """
         ...
 
